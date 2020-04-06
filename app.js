21269f97a65633ac4a623208354755a00f78aadd
Robbin van Ooij <robbin@vanooij.nl>
2020-04-06 20:32:33 +0200

    fixed the audio looping and preloading issue

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ let offset = 0; // initial value of image offset within each slice
 let miniMap, sky, title, ball;
 
 const state = {
-    scene: Scenes.LOADING
+    scene: Scenes.LOADING,
+    musicPlaying: false,
+    loadingAsset: ''
 };
 
 /**
@@ -57,13 +59,43 @@ images.map(image => {
 });
 
 /**
+ * Holds all sounds used in the app
+ * @param {string} id - id to refer to the image
+ * @param {string} src - path to the image
+ * @param {Object} audio - the audio object created after initialisation
+ */
+const sounds = [
+    {
+        id: 'title_music',
+        src: 'assets/golf_short.wav',
+        audio: new Audio()
+    }
+];
+
+/**
+ * Loads sound asset to each sound object inside sounds
+ */
+sounds.map(sound => {
+    sound.audio.src = sound.src;
+    sound.audio.preload = "auto";
+});
+
+/**
  * Checks to see if all image assets are loading by checking the dimensions of the loaded bitmap
  * @returns {boolean} assetsLoaded
  */
 function assetsLoaded() {
     for (let loadedBitmap = 0; loadedBitmap < images.length; loadedBitmap ++) {
         if (!images[loadedBitmap] || images[loadedBitmap].img.naturalWidth <= 0) {
+            state.loadingAsset = images[loadedBitmap].src;
 	        return false;
+        }
+    }
+
+    for (let loadedSound = 0; loadedSound < sounds.length; loadedSound ++) {
+        if (!sounds[loadedSound].audio || !sounds[loadedSound].audio.duration) {
+            state.loadingAsset = sounds[loadedSound].src;
+            return false;
         }
     }
 
@@ -88,6 +120,35 @@ function init() {
 	title = new Title(context, images.filter(img => img.id === 'title')[0]);
 }
 
+function playSound(sound, loop = false) {
+    const soundToPlay = sounds.filter(soundObj => soundObj.id === sound);
+
+    if (loop) {
+        soundToPlay[0].audio.loop = true;
+
+        // experimental attempt to shave a few ms off the duration in order to force a more seamless loop
+        soundToPlay[0].audio.addEventListener('timeupdate', function() {
+        let buffer = .275; // a bit magic
+        if (soundToPlay[0].audio.currentTime > soundToPlay[0].audio.duration - buffer) {
+
+            // restart sound (this does not use the built-in loop function since that is bugged)
+            soundToPlay[0].audio.currentTime = 0;
+            soundToPlay[0].audio.position = 0;
+            soundToPlay[0].audio.play();
+        }
+        }, false);
+    }
+
+    soundToPlay[0].audio.muted = false;
+    soundToPlay[0].audio.play();
+}
+
+function stopSound(sound) {
+    const soundToStop = sounds.filter(soundObj => soundObj.id === sound);
+    soundToStop[0].audio.position = 0;
+    soundToStop[0].audio.muted = true;
+}
+
 function drawBitmapSlices() {
 
     const cfg = {
@@ -107,6 +168,10 @@ function loader() {
     context.font = "30px Arial";
     context.fillStyle='#ffffff';
     context.fillText("LOADING", (800 / 2) - (context.measureText("LOADING").width / 2), 250);
+
+    context.font = "12px Arial";
+    context.fillStyle='#ffffff';
+    context.fillText(state.loadingAsset, (800 / 2) - (context.measureText(state.loadingAsset).width / 2), 280);
 
     // show bouncing ball when image is loaded
     if (ball) {
@@ -137,6 +202,10 @@ function update() {
             loader();
             break;
         case Scenes.TITLE:
+            if (!state.musicPlaying) {
+                state.musicPlaying = true;
+                playSound('title_music', true);
+            }
             clearCanvas();
             sky.draw(); // this may be dropped from final version
             title.draw();
